880d26f1ef22d22d2c983cc54042fb48ddbffa6d
GabrielaAlura <106533411+GabrielaAlura@users.noreply.github.com>
2023-11-30 07:58:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio) .play();
     }
     const listadeTeclas = document.querySelectorAll(".tecla");
-   
+    
     let contador = 0;
     while(contador<listadeTeclas.length){
         const efeito = listadeTeclas[contador].classList[1];
